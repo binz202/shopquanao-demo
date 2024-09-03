7066c6e0e0d4b31a740697e80da8759926257638
binz202 <132970047+binz202@users.noreply.github.com>
2024-09-03 16:53:11 +0700

    upload

---
+++ login/login.js
@@ -0,0 +1,13 @@
+
+x=document.querySelector('.fa-bars')
+x.addEventListener('click',function(){
+    // console.log('hello word')
+     document.querySelector('.gt'). classList.toggle('khac');
+})
+
+// console.log('helo')
+
+y=document.querySelector('.gt')
+y.addEventListener('click',function(){
+    // console.log('helo')
+})
---
+++ shopdo_nam/shop_nam.js
@@ -0,0 +1,42 @@
+function showContent() {
+    var hiddenContent = document.querySelector('#hidden-content');
+    hiddenContent.classList.toggle('hidden'); // Chuyển đổi lớp 'hidden'
+  }
+//   window.onload = function() {
+//     alert("Xin chào! Chúng tôi chào mừng bạn đến với cửa hàng của chúng tôi.");
+// };
+window.addEventListener('DOMContentLoaded', function() {
+  var notificationDiv = document.getElementById('notificationDiv');
+  var closeBtn = document.getElementById('closeBtn');
+  
+  // Hiển thị hộp thông báo ngay khi trang web đã được tải lại
+  notificationDiv.classList.remove('hidden');
+  
+  // Xử lý sự kiện click vào nút close
+  closeBtn.addEventListener('click', function() {
+    notificationDiv.classList.add('hidden');
+  });
+});
+// end
+var myButton = document.getElementById("myButton");
+var myDiv = document.getElementById("myDiv");
+
+myButton.addEventListener("click", function() {
+  if (myDiv.style.display === "none") {
+    myDiv.style.display = "block";
+    myButton.innerHTML = "";
+  } else {
+    myDiv.style.display = "none";
+    myButton.innerHTML = "";
+  }
+});
+// end
+var marquee = document.querySelector('marquee');
+
+marquee.addEventListener('mouseover', function () {
+    marquee.stop();
+});
+
+marquee.addEventListener('mouseout', function () {
+    marquee.start();
+});
